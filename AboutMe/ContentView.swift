013ad6d2ead589ac77be78465e7f6fb6059cca6c
kwk Cecilia .C <cc.comar36@gmail.com>
2025-07-23 13:36:19 -0400

    Created stacks (Z,V,H), Title Name, main pic of me, & caption

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            VStack {
+                Text("Cecilia")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.419, saturation: 0.819, brightness: 0.358))
+                    .padding(0.0)
+                Image("July4-PicOfMe")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    
+                HStack{
+                    Text("Here are some of my pics of summer 2025 so far:")
+                        .font(.title2)
+                        .foregroundColor(Color(hue: 0.228, saturation: 0.853, brightness: 0.4))
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
