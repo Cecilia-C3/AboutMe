1c3cbcddd6902af5a8cb33023a32179d39ca9b0d
kwk Cecilia .C <cc.comar36@gmail.com>
2025-07-23 14:01:31 -0400

    Added two more images at the bottom side-to-side

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -10,20 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            VStack {
+            
+            VStack (alignment: .leading, spacing: 30.00){
                 Text("Cecilia")
                     .font(.largeTitle)
                     .fontWeight(.bold)
                     .foregroundColor(Color(hue: 0.419, saturation: 0.819, brightness: 0.358))
-                    .padding(0.0)
+                    .padding()
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    
+                    
                 Image("July4-PicOfMe")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     
+                Text("Here are some of my pics of summer 2025 so far:")
+                    .font(.title2)
+                    .foregroundColor(Color(hue: 0.228, saturation: 0.853, brightness: 0.4))
+                    .multilineTextAlignment(.center)
+                
                 HStack{
-                    Text("Here are some of my pics of summer 2025 so far:")
-                        .font(.title2)
-                        .foregroundColor(Color(hue: 0.228, saturation: 0.853, brightness: 0.4))
+                    
+        
+                    Image("Trail-Pic")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Image("Met-Pic")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                
                 }
             }
             .padding()
